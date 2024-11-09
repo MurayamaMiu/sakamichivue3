ec9126d5d17c26e3718377eaf7e2f02905263555
西宮ソラ <kohojp@outlook.jp>
2024-11-09 21:15:03 +0800

    11.9

--- src/App.vue
+++ src/App.vue
@@ -133,12 +133,12 @@
         <div class="offcanvas-body">
           <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="./">{{
+              <a class="nav-link active" aria-current="page" href="#/">{{
                 $t('home')
               }}</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="./musicplayer">{{
+              <a class="nav-link" href="#/musicplayer">{{
                 $t('musicplayer')
               }}</a>
             </li>
